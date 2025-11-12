53a8647d1659fa0273ae29d2c1391da0e2c925e9
Kashinath Kambar <kambarkashinath19@gmail.com>
2025-11-12 11:30:18 +0530

    Update vite.config.js

--- my-flowchart-app/vite.config.js
+++ my-flowchart-app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 required for correct asset paths on GitHub Pages
 })
