a9518e4c6473a4cb076aa95515cddcdafa4f0e1d
Kashinath Kambar <kambarkashinath19@gmail.com>
2025-11-12 11:31:41 +0530

    Create About.jsx

---
+++ my-flowchart-app/src/About.jsx
@@ -0,0 +1,11 @@
+// src/About.jsx
+function About() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>About Page</h1>
+      <p>Welcome to my Anime site built with React + Vite!</p>
+    </div>
+  );
+}
+
+export default About;
